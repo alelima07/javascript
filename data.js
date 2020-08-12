6cfbb43ac26d267b259de5c57ba5f5e5353a8253
alelima07 <alelima07@gmail.com>
2020-08-12 15:24:24 -0700

    added views detail handlebars homehandlebars gitignore and express

--- data.js
+++ data.js
@@ -1,30 +1,30 @@
 let books = [
     {
-        book: 'Harry Potter and the Prisoner of Azkaban',
+        title: 'Harry Potter and the Prisoner of Azkaban',
         author: 'J K Rowling',
         year: 1999,
         ranking: 1
     }, 
     {
-        book: 'Harry Potter and the Deathly Hallows',
+        title: 'Harry Potter and the Deathly Hallows',
         author: 'J K Rowling',
         year: 2007,
         ranking: 2
     }, 
     {
-        book: 'Harry Potter and the Half-Blood Prince',
+        title: 'Harry Potter and the Half-Blood Prince',
         author: 'J K Rowling',
         year: 2005,
         ranking: 3
     }, 
     {
-        book: 'Harry Potter and the Order of the Phoenix',
+        title: 'Harry Potter and the Order of the Phoenix',
         author: 'J K Rowling',
         year: 2003,
         ranking: 4
     }, 
     {
-        book: 'Harry Potter and the Sorcerer Stone',
+        title: 'Harry Potter and the Sorcerer Stone',
         author: 'J K Rowling',
         year: 1997,
         ranking: 5
@@ -32,4 +32,39 @@ let books = [
 ];
 exports.getAll = () => {
 return books;
+}
+
+
+// export method that returns full data about the requested item
+exports.getItem = title => {
+    const selectBook = books.find(books => books.title === title); 
+    if (selectBook === undefined){
+        return {status: false, message: "book does not exist"};
+    }
+    else{
+        return {status: true, message:"Book found in array", book: selectBook};
+    }
+}
+
+// export method that adds a new item to the data array, if it doesn't already exist
+exports.addItem = (newBook) => {
+    let bookIndex = books.findIndex(books => books.title === newBook.title);
+    if(bookIndex != -1) { 
+        return {status: false, message: "book already exists"};
+    }
+    else{ //if it doesn't already exist, add it to the array
+        books.push(newBook);
+        return {status: true, message: "new book successfully added to the array"};
+    }
+} 
+
+//export method that deletes the requested item
+exports.deleteItem = title => {
+    let bookIndex = books.findIndex(books => books.title === title);
+    if(bookIndex != -1) {
+        books.splice(bookIndex, 1);
+        return {status: true, message: "Book successfully deleted from the array"};
+    }
+    return {status: false, message: "Book does not exist in the array"}
+    
 }
--- index.js
+++ index.js
@@ -1,4 +1,67 @@
-const http = require('http');
+// Alessandra Souza Lima
+// Course: IT122  (Week 2 - Express Yourself)
+// File: index.js
+
+'use strict'
+const express = require("express");
+const exphbs = require('express-handlebars');
+const bodyParser = require("body-parser")
+const app = express();
+const selectBook = require('./data.js'); // reference the information in the data.js file
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); // set location for static files
+app.use(bodyParser.urlencoded({extended: true})); // parse form submissions
+
+app.engine('handlebars', exphbs({defaultLayout: false}));
+app.set("view engine", "handlebars");
+
+/* Is it really selectBookItems? */
+const selectBookItems = selectBook.getAll(); // get all of the books items
+
+// view homepage
+app.get('/', (req, res) => {
+  res.type('text/html');
+  res.render('home', {selectBook: selectBookItems});
+ });
+
+ // view detail page
+ app.get('/detail', (req, res) => {
+  const title = req.query.book; //query the book that was clicked
+  console.log(title); 
+  const displayBook = selectBook.getItem(title); //get all details for the book
+  console.log(displayBook);
+  res.type('text/html');
+  res.render('detail', {title: title, details: displayBook.book}); 
+ });
+ 
+ // view about page
+ app.get('/about', (req, res) => {
+  const aboutMe = `About Me: \n My name is Alessandra Lima and I am a full time student at Seattle Central studying Programming.`;
+  res.type('text/plain');
+  res.send(`${aboutMe}`);
+ });
+
+ // define 404 handler
+app.use( (req,res) => {
+  res.type('text/plain'); 
+  res.status(404);
+  res.send('404 Error - Page not found');
+ });
+
+ // start the web server
+ app.listen(app.get('port'), () => {
+  console.log(`Express started`); 
+ });
+
+
+
+
+
+
+//Code Assignment 1 //
+
+/* const http = require('http');
 const books = require('./data.js');
 let booksItems = books.getAll();
 
@@ -24,7 +87,7 @@ http.createServer((req, res) => {
       res.end('404 Error - Page not found');
       break;
     }
-}).listen(process.env.PORT || 3000);
+}).listen(process.env.PORT || 3000); */
             
 
     
